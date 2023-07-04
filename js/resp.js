a473f067b3250302b604009843b2ae06d6f19cdc
Sanket Chandrakant Belge <97460462+sanketbelge@users.noreply.github.com>
2023-07-04 14:16:01 +0530

    Update resp.js

--- js/resp.js
+++ js/resp.js
@@ -10,3 +10,20 @@ burger.addEventListener('click', ()=>{
     navbar.classList.toggle('h-nav-resp');
     
 })
+//scipt js//
+
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if(bar){
+    bar.addEventListener('click',()=>{
+        nav.classList.add('active');
+    })
+}
+
+if(close){
+    close.addEventListener('click',()=>{
+        nav.classList.remove('active');
+    })
+}
